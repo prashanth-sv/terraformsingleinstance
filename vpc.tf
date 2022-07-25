82d278f523ee1f5bf8205a11b765880da7d19992
prashanth-sv <yuhwyesh@gmail.com>
2022-07-25 23:22:28 +0530

    single instance code

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-	Owner = "Prashanth-svvv"
-	environment = "${var.environment}"
-    }
-}
