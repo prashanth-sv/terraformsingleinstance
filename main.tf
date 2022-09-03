5561afdafd43d98115975ebf6451ebf27a2b60f1
prashanth-sv <109160025+prashanth-sv@users.noreply.github.com>
2022-09-03 18:45:19 +0530

    Update main.tf
    Only Vpc code  working

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #This Terraform Code Deploys Basic  Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+#     access_key = "${var.aws_access_key}"
+#     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 
@@ -122,29 +122,29 @@ resource "aws_security_group" "allow_all" {
 # }
 
 
-resource "aws_instance" "web-1" {
-    ami = var.imagename
-    #ami = "ami-0d857ff0f5fc4e03b"
-    #ami = "${data.aws_ami.my_ami.id}"
-    availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
-    key_name = "ALL"
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true
-    tags = {
-        Name = "Server-1"  #tag name for server
-        Env = "Prod"
-        Owner = "Prath-sv"
-      CostCenter = "ABCD"
-    }
-    user_data = <<-EOF
-     #!/bin/bash
-     sudo apt-get update
-     sudo apt-get install -y nginx jq stress
-     echo "<h1>Deployed via Terraform</h1>" | sudo tree /var/www/html/index.html
-     EOF
-}
+# resource "aws_instance" "web-1" {
+#     ami = var.imagename
+#     #ami = "ami-0d857ff0f5fc4e03b"
+#     #ami = "${data.aws_ami.my_ami.id}"
+#     availability_zone = "us-east-1a"
+#     instance_type = "t2.micro"
+#     key_name = "ALL"
+#     subnet_id = "${aws_subnet.subnet1-public.id}"
+#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+#     associate_public_ip_address = true
+#     tags = {
+#         Name = "Server-1"  #tag name for server
+#         Env = "Prod"
+#         Owner = "Prath-sv"
+#       CostCenter = "ABCD"
+#     }
+#     user_data = <<-EOF
+#      #!/bin/bash
+#      sudo apt-get update
+#      sudo apt-get install -y nginx jq stress
+#      echo "<h1>Deployed via Terraform</h1>" | sudo tree /var/www/html/index.html
+#      EOF
+# }
 
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"
